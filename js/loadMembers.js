b6d9281eca8c25c492ad4684a0cd32515d14235d
brunoartc <brunoartc@yahoo.com>
2020-04-11 04:06:45 -0300

    add custom avatar

--- js/loadMembers.js
+++ js/loadMembers.js
@@ -1,3 +1,15 @@
+function imageExists(image_url){
+
+    var http = new XMLHttpRequest();
+
+    http.open('HEAD', image_url, false);
+    http.send();
+
+    return http.status != 404;
+
+}
+
+
 function loadMembers() {
 
 
@@ -7,6 +19,8 @@ function loadMembers() {
     members.class = "flex-container"
 
     members.id = "members"
+
+    
 
 
     $.get("https://api.github.com/orgs/BlockchainInsper/public_members", function (data) {
@@ -20,16 +34,34 @@ function loadMembers() {
 
 
             let { login, avatar_url } = data[index]
-
             position = "Babaca"
 
-            let template = `<div id="${login}" class="box";">
+            let template;
+            
+
+            if (imageExists(`img/avatar/${login}.jpg`)){
+                template = `<div id="${login}" class="box";">
                                 <div class="image-round">
-                                    <img src="${avatar_url}" alt="Person 1">
+                                    <img src="img/avatar/${login}.jpg" alt="Person 1">
                                 </div>
                                 <h3 id="name">${login}</h3>
                                 <p id="position">${position}</p>
                             </div>`
+                
+            } else {
+                template = `<div id="${login}" class="box";">
+                                    <div class="image-round">
+                                        <img src="${avatar_url}" alt="Person 1">
+                                    </div>
+                                    <h3 id="name">${login}</h3>
+                                    <p id="position">${position}</p>
+                                </div>`
+
+            }
+
+            
+
+            
 
 
 
