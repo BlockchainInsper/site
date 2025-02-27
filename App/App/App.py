69b698503499864692b8aeaebb8eae430246bf1a
matheusluciano99 <matheusluciano99@gmail.com>
2025-02-27 11:56:26 -0300

    refatorando intro

--- App/App/App.py
+++ App/App/App.py
@@ -7,6 +7,7 @@ from components.home.Hero import call_to_action_with_video
 from components.home.Features import features
 from components.home.Testimonials import testimonials
 from components.home.With_Background_Image import with_background_image
+from components.processo_seletivo.Intro import intro
 
 
 class State(rx.State):
@@ -37,137 +38,7 @@ def processo_seletivo():
         rx.box(
             rx.box(
                 navbar(),
-                rx.box(
-                    rx.box(
-                        rx.box(
-                            rx.box(
-                                rx.heading(
-                                    "Nosso Processo Seletivo chegou!",
-                                    class_name="css-kttl9l",
-                                    as_="h2",
-                                    size="6",
-                                ),
-                                rx.el.a(
-                                    rx.el.button(
-                                        "Inscrever-se",
-                                        type="button",
-                                        class_name="chakra-button css-b85hn3",
-                                    ),
-                                    target="_blank",
-                                    rel="noopener noreferrer",
-                                    class_name="chakra-link css-f4h6uy",
-                                    href="https://docs.google.com/forms/d/e/1FAIpQLScQyW4RGcwtcNAAVtYj3iFJdgJ4Khq07rSluQo5tWROKWYxow/viewform",
-                                ),
-                                rx.el.a(
-                                    rx.el.button(
-                                        rx.text.span(
-                                            rx.el.svg(
-                                                rx.el.svg.path(
-                                                    d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z"
-                                                ),
-                                                stroke="currentColor",
-                                                fill="currentColor",
-                                                stroke_width="0",
-                                                role="img",
-                                                viewbox="0 0 24 24",
-                                                aria_hidden="true",
-                                                focusable="false",
-                                                height="1em",
-                                                width="1em",
-                                                xmlns="http://www.w3.org/2000/svg",
-                                            ),
-                                            class_name="chakra-button__icon css-1wh2kri",
-                                        ),
-                                        "Discord",
-                                        type="button",
-                                        class_name="chakra-button css-a6md8f",
-                                    ),
-                                    target="_blank",
-                                    rel="noopener noreferrer",
-                                    class_name="chakra-link css-f4h6uy",
-                                    href="https://discord.gg/jdK5yB48Mm",
-                                ),
-                                class_name="chakra-stack css-1lpczhs",
-                            ),
-                            class_name="css-zd4of5",
-                        ),
-                        rx.box(
-                            rx.box(
-                                rx.text(
-                                    "O maior experimento financeiro e sociol\u00f3gico do s\u00e9culo XXI, o Bitcoin veio solucionar diversos problemas antes sem solu\u00e7\u00e3o. A Blockchain Insper vai te dar base para entender o que est\u00e1 por tr\u00e1s da constru\u00e7\u00e3o do protocolo e o que motivou Satoshi Nakamoto a desenvolv\u00ea-lo, entre muitas outras coisas que rondam essa tecnologia que est\u00e1 revolucionando a economia mundial. Al\u00e9m disso, temos parcerias com as maiores empresas do mercado para, al\u00e9m de ganhar conhecimento, saber como funcionam os projetos na pr\u00e1tica.",
-                                    class_name="css-0",
-                                ),
-                                class_name="css-k008qs",
-                            ),
-                            class_name="css-0",
-                        ),
-                        class_name="css-1xb0s7",
-                    ),
-                    rx.divider(
-                        aria_orientation="horizontal",
-                        class_name="chakra-divider css-gir5mk",
-                    ),
-                    rx.box(
-                        rx.box(
-                            rx.heading(
-                                "Primeira Fase",
-                                class_name="css-vv36la",
-                                as_="h3",
-                                size="4",
-                            ),
-                            rx.text(
-                                "Preenchimento de um Forms com detalhes pessoais e perguntas de car\u00e1ter opinativo.",
-                                class_name="css-0",
-                            ),
-                            class_name="css-0",
-                        ),
-                        rx.box(
-                            rx.heading(
-                                "Segunda Fase",
-                                class_name="css-vv36la",
-                                as_="h3",
-                                size="4",
-                            ),
-                            rx.text(
-                                "Resolu\u00e7\u00e3o de um case elaborado em parceria com o BTG e com a Mynt.",
-                                class_name="css-0",
-                            ),
-                            class_name="css-0",
-                        ),
-                        rx.box(
-                            rx.heading(
-                                "Terceira Fase",
-                                class_name="css-vv36la",
-                                as_="h3",
-                                size="4",
-                            ),
-                            rx.text(
-                                "Entrevista individual para conhecer melhor o candidato com perguntas pessoais e possivelmente t\u00e9cnicas.",
-                                class_name="css-0",
-                            ),
-                            class_name="css-0",
-                        ),
-                        rx.box(
-                            rx.heading(
-                                "Programa de Trainees",
-                                class_name="css-vv36la",
-                                as_="h3",
-                                size="4",
-                            ),
-                            rx.text(
-                                "Desempenho no Curso de Introdu\u00e7\u00e3o \u00e0 Blockchain e Projeto Interno Aplicado (nessa fase, os candidatos j\u00e1 ganham entre 5 e 10 horas complementares).",
-                                class_name="css-0",
-                            ),
-                            class_name="css-0",
-                        ),
-                        class_name="css-1gmb2s4",
-                    ),
-                    rx.divider(
-                        aria_orientation="horizontal",
-                        class_name="chakra-divider css-gir5mk",
-                    ),
-                    class_name="chakra-container css-1x6bs2p",
-                ),
+                intro(),
                 rx.box(
                     rx.box(
                         rx.el.a(
