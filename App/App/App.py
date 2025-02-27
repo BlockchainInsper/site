eaa6a4c783e2f48bf51526d1e0135c8bf6315dc1
matheusluciano99 <matheusluciano99@gmail.com>
2025-02-27 11:34:17 -0300

    refactoring WithBackGroundImage

--- App/App/App.py
+++ App/App/App.py
@@ -6,6 +6,7 @@ from components.Footer import footer
 from components.home.Hero import call_to_action_with_video
 from components.home.Features import features
 from components.home.Testimonials import testimonials
+from components.home.With_Background_Image import with_background_image
 
 
 class State(rx.State):
@@ -23,31 +24,7 @@ def index():
                 call_to_action_with_video(),
                 features(),
                 testimonials(),
-                rx.box(
-                    rx.box(
-                        rx.box(
-                            rx.text(
-                                "Conhe\u00e7a nossas \u00e1reas! Estamos estruturados em \u00e1reas de estudo e \u00e1reas administrativas",
-                                class_name="chakra-text css-y5rwul",
-                            ),
-                            rx.box(
-                                rx.el.a(
-                                    rx.el.button(
-                                        "Nossas \u00e1reas",
-                                        type="button",
-                                        class_name="chakra-button css-1onh796",
-                                    ),
-                                    class_name="chakra-link css-f4h6uy",
-                                    href="#/areas",
-                                ),
-                                class_name="chakra-stack css-x9juev",
-                            ),
-                            class_name="chakra-stack css-7nla9k",
-                        ),
-                        class_name="chakra-stack css-13v4fef",
-                    ),
-                    class_name="css-1utqpha",
-                ),
+                with_background_image(),
                 footer(),
                 id="root",
             )
