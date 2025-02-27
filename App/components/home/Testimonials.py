b4ab1d1a3e749e2f0cdb7208197760696e91560f
matheusluciano99 <matheusluciano99@gmail.com>
2025-02-27 11:33:33 -0300

    refactoring testimonials

---
+++ App/components/home/Testimonials.py
@@ -0,0 +1,120 @@
+import reflex as rx
+
+
+def testimonials():
+    return (
+        rx.box(
+            rx.box(
+                rx.box(
+                    rx.heading(
+                        "Depoimentos de nossos membros",
+                        class_name="chakra-heading css-1dklj6k",
+                        as_="h2",
+                        size="6",
+                    ),
+                    class_name="chakra-stack css-jp20jv",
+                ),
+                rx.box(
+                    rx.box(
+                        rx.box(
+                            rx.text(
+                                "Hoje acredito que a entidade se tornou algo muito mais pr\u00f3ximo do que imagin\u00e1vamos quando foi fundada, um organismo que funciona de maneira independente de qualquer membro espec\u00edfico. Al\u00e9m da possibilidade de aprender e debater com pessoas inteligentes sobre caminhos futuros para a sociedade por meio da tecnologia, os membros tem a oportunidade de aplicar essas ideias na pr\u00e1tica nas \u00e1reas internas e tamb\u00e9m em projetos com as principais empresas do pa\u00eds como Ambev e BTG Pactual.",
+                                class_name="chakra-text css-16ozagw",
+                            ),
+                            class_name="chakra-stack css-1ahniv6",
+                        ),
+                        rx.box(
+                            rx.text.span(
+                                rx.image(
+                                    src="/static/media/felipe.b7224a86.jpeg",
+                                    class_name="chakra-avatar__img css-3a5bz2",
+                                ),
+                                alt="Felipe Santos",
+                                class_name="chakra-avatar css-in8e35",
+                            ),
+                            rx.box(
+                                rx.text(
+                                    "Felipe Santos",
+                                    class_name="chakra-text css-35ezg3",
+                                ),
+                                rx.text(
+                                    "Co-fundador e Ex-membro",
+                                    class_name="chakra-text css-zgbaaa",
+                                ),
+                                class_name="chakra-stack css-130k5dj",
+                            ),
+                            class_name="css-1p6ys5y",
+                        ),
+                        class_name="css-0",
+                    ),
+                    rx.box(
+                        rx.box(
+                            rx.text(
+                                "Quando me chamaram e disseram que estavam fazendo uma entidade relacionada a isso eu vi uma oportunidade de disseminar o conhecimento nem que fosse dentro do pr\u00f3prio insper Foi ent\u00e3o que me juntei ao time de fundadores da entidade. Com uma miss\u00e3o de difundir o conhecimento e fazer com que as pessoas gostem de aprender e tenham as melhores ferramentas a sua disposi\u00e7\u00e3o. Por isso decido fazer v\u00e1rios projetos para que eu possa levar o conhecimento que fui adquirindo para os outros seja na forma de aulas, ou at\u00e9 mesmo mentira do um projeto proposto.",
+                                class_name="chakra-text css-16ozagw",
+                            ),
+                            class_name="chakra-stack css-1ahniv6",
+                        ),
+                        rx.box(
+                            rx.text.span(
+                                rx.image(
+                                    src="/static/media/bruno.34122a46.jpeg",
+                                    class_name="chakra-avatar__img css-3a5bz2",
+                                ),
+                                alt="Bruno Arthur",
+                                class_name="chakra-avatar css-in8e35",
+                            ),
+                            rx.box(
+                                rx.text(
+                                    "Bruno Arthur",
+                                    class_name="chakra-text css-35ezg3",
+                                ),
+                                rx.text(
+                                    "Co-fundador e Ex-membro",
+                                    class_name="chakra-text css-zgbaaa",
+                                ),
+                                class_name="chakra-stack css-130k5dj",
+                            ),
+                            class_name="css-1p6ys5y",
+                        ),
+                        class_name="css-0",
+                    ),
+                    rx.box(
+                        rx.box(
+                            rx.text(
+                                "Fundar a B.I. foi um desafio \u00edmpar. Estudar uma tecnologia t\u00e3o latente e nova trouxe desafios extras, mas ao mesmo tempo diferenciais competitivos em nossos curr\u00edculos, logo no in\u00edcio de nossas carreiras. Habilidades de aprendizado, gest\u00e3o de equipe, resolu\u00e7\u00e3o de conflitos, entendimento de viabilidade de projetos e tomada de decis\u00e3o, eram desenvolvidas a cada dia. Hoje posso falar que a entidade teve papel fundamental em meu desenvolvimento profissional e na posi\u00e7\u00e3o que ocupo hoje.",
+                                class_name="chakra-text css-16ozagw",
+                            ),
+                            class_name="chakra-stack css-1ahniv6",
+                        ),
+                        rx.box(
+                            rx.text.span(
+                                rx.image(
+                                    src="/static/media/joao.8543876f.jpeg",
+                                    class_name="chakra-avatar__img css-3a5bz2",
+                                ),
+                                alt="Jo\u00e3o P. J. M. Perpetuo",
+                                class_name="chakra-avatar css-in8e35",
+                            ),
+                            rx.box(
+                                rx.text(
+                                    "Jo\u00e3o P. J. M. Perpetuo",
+                                    class_name="chakra-text css-35ezg3",
+                                ),
+                                rx.text(
+                                    "Co-fundador e Ex-membro",
+                                    class_name="chakra-text css-zgbaaa",
+                                ),
+                                class_name="chakra-stack css-130k5dj",
+                            ),
+                            class_name="css-1p6ys5y",
+                        ),
+                        class_name="css-0",
+                    ),
+                    class_name="chakra-stack css-1k0eqqv",
+                ),
+                class_name="chakra-container chakra-stack css-1cvohfv",
+            ),
+            class_name="css-1cozlc8",
+        ),
+    )
