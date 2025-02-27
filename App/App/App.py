e3dc3b86416b9864ee6e7aa8234faa535f4abd1b
matheusluciano99 <matheusluciano99@gmail.com>
2025-02-27 11:23:12 -0300

    refactoring features

--- App/App/App.py
+++ App/App/App.py
@@ -4,6 +4,7 @@ import reflex as rx
 from components.Navbar import navbar
 from components.Footer import footer
 from components.home.Hero import call_to_action_with_video
+from components.home.Features import features
 
 
 class State(rx.State):
@@ -19,148 +20,7 @@ def index():
             rx.box(
                 navbar(),
                 call_to_action_with_video(),
-                rx.box(
-                    rx.box(
-                        rx.box(
-                            rx.heading(
-                                "Reavaliando o presente e construindo o futuro",
-                                class_name="chakra-heading css-1dklj6k",
-                                as_="h2",
-                                size="6",
-                            ),
-                            rx.text(
-                                "Buscamos criar um time altamente engajado e preparado para enfrentar a nova onda de tecnologia no mercado de trabalho. Seguimos com o objetivo de estimular o estudo e a ado\u00e7\u00e3o dessa tecnologia no Brasil, criando conhecimento n\u00e3o apenas para o agora, como tamb\u00e9m para o futuro.",
-                                class_name="chakra-text css-ef8b8e",
-                            ),
-                            rx.box(
-                                rx.box(
-                                    rx.box(
-                                        rx.box(
-                                            rx.el.svg(
-                                                rx.el.svg.path(
-                                                    fill="none",
-                                                    stroke_linecap="round",
-                                                    stroke_linejoin="round",
-                                                    stroke_width="32",
-                                                    d="M461.81 53.81a4.4 4.4 0 00-3.3-3.39c-54.38-13.3-180 34.09-248.13 102.17a294.9 294.9 0 00-33.09 39.08c-21-1.9-42-.3-59.88 7.5-50.49 22.2-65.18 80.18-69.28 105.07a9 9 0 009.8 10.4l81.07-8.9a180.29 180.29 0 001.1 18.3 18.15 18.15 0 005.3 11.09l31.39 31.39a18.15 18.15 0 0011.1 5.3 179.91 179.91 0 0018.19 1.1l-8.89 81a9 9 0 0010.39 9.79c24.9-4 83-18.69 105.07-69.17 7.8-17.9 9.4-38.79 7.6-59.69a293.91 293.91 0 0039.19-33.09c68.38-68 115.47-190.86 102.37-247.95zM298.66 213.67a42.7 42.7 0 1160.38 0 42.65 42.65 0 01-60.38 0z",
-                                                ),
-                                                rx.el.svg.path(
-                                                    fill="none",
-                                                    stroke_linecap="round",
-                                                    stroke_linejoin="round",
-                                                    stroke_width="32",
-                                                    d="M109.64 352a45.06 45.06 0 00-26.35 12.84C65.67 382.52 64 448 64 448s65.52-1.67 83.15-19.31A44.73 44.73 0 00160 402.32",
-                                                ),
-                                                stroke="currentColor",
-                                                fill="currentColor",
-                                                stroke_width="0",
-                                                viewbox="0 0 512 512",
-                                                focusable="false",
-                                                class_name="chakra-icon css-1lbb8sh",
-                                                height="1em",
-                                                width="1em",
-                                                xmlns="http://www.w3.org/2000/svg",
-                                            ),
-                                            class_name="css-1d7t0og",
-                                        ),
-                                        rx.text(
-                                            "Nossa Miss\u00e3o",
-                                            class_name="chakra-text css-35ezg3",
-                                        ),
-                                        class_name="chakra-stack css-84zodg",
-                                    ),
-                                    rx.text(
-                                        "Fomentar o desenvolvimento do ecossistema brasileiro em torno da tecnologia blockchain, criando um futuro mais eficiente atrav\u00e9s da tecnologia.",
-                                        class_name="chakra-text css-1tngv1n",
-                                    ),
-                                    class_name="css-0",
-                                ),
-                                rx.box(class_name="chakra-stack__divider css-1uymzum"),
-                                rx.box(
-                                    rx.box(
-                                        rx.box(
-                                            rx.el.svg(
-                                                rx.el.svg.circle(
-                                                    cx="256", cy="256", r="64"
-                                                ),
-                                                rx.el.svg.path(
-                                                    d="M490.84 238.6c-26.46-40.92-60.79-75.68-99.27-100.53C349 110.55 302 96 255.66 96c-42.52 0-84.33 12.15-124.27 36.11-40.73 24.43-77.63 60.12-109.68 106.07a31.92 31.92 0 00-.64 35.54c26.41 41.33 60.4 76.14 98.28 100.65C162 402 207.9 416 255.66 416c46.71 0 93.81-14.43 136.2-41.72 38.46-24.77 72.72-59.66 99.08-100.92a32.2 32.2 0 00-.1-34.76zM256 352a96 96 0 1196-96 96.11 96.11 0 01-96 96z"
-                                                ),
-                                                stroke="currentColor",
-                                                fill="currentColor",
-                                                stroke_width="0",
-                                                viewbox="0 0 512 512",
-                                                focusable="false",
-                                                class_name="chakra-icon css-1oovkcf",
-                                                height="1em",
-                                                width="1em",
-                                                xmlns="http://www.w3.org/2000/svg",
-                                            ),
-                                            class_name="css-98kz1g",
-                                        ),
-                                        rx.text(
-                                            "Nossa Vis\u00e3o",
-                                            class_name="chakra-text css-35ezg3",
-                                        ),
-                                        class_name="chakra-stack css-84zodg",
-                                    ),
-                                    rx.text(
-                                        "Capacitar os alunos com o melhor conte\u00fado e conect\u00e1-los ao mercado, no intuito de incluir nosso pa\u00eds nesse cen\u00e1rio de inova\u00e7\u00e3o.",
-                                        class_name="chakra-text css-1tngv1n",
-                                    ),
-                                    class_name="css-0",
-                                ),
-                                rx.box(class_name="chakra-stack__divider css-1uymzum"),
-                                rx.box(
-                                    rx.box(
-                                        rx.box(
-                                            rx.el.svg(
-                                                rx.el.svg.circle(
-                                                    cx="256", cy="256", r="24"
-                                                ),
-                                                rx.el.svg.path(
-                                                    d="M256 48C141.31 48 48 141.31 48 256s93.31 208 208 208 208-93.31 208-208S370.69 48 256 48zm105.07 113.33l-46.88 117.2a64 64 0 01-35.66 35.66l-117.2 46.88a8 8 0 01-10.4-10.4l46.88-117.2a64 64 0 0135.66-35.66l117.2-46.88a8 8 0 0110.4 10.4z"
-                                                ),
-                                                stroke="currentColor",
-                                                fill="currentColor",
-                                                stroke_width="0",
-                                                viewbox="0 0 512 512",
-                                                focusable="false",
-                                                class_name="chakra-icon css-7gskat",
-                                                height="1em",
-                                                width="1em",
-                                                xmlns="http://www.w3.org/2000/svg",
-                                            ),
-                                            class_name="css-izs6lo",
-                                        ),
-                                        rx.text(
-                                            "Nossos Valores",
-                                            class_name="chakra-text css-35ezg3",
-                                        ),
-                                        class_name="chakra-stack css-84zodg",
-                                    ),
-                                    rx.text(
-                                        "Alto comprometimento, proatividade, inova\u00e7\u00e3o, trabalho em equipe, multidisciplinaridade, excel\u00eancia e efici\u00eancia.",
-                                        class_name="chakra-text css-1tngv1n",
-                                    ),
-                                    class_name="css-0",
-                                ),
-                                class_name="chakra-stack css-j7qwjs",
-                            ),
-                            class_name="chakra-stack css-egoftb",
-                        ),
-                        rx.box(
-                            rx.image(
-                                alt="feature image",
-                                src="/static/media/insper.938cda6f.jpg",
-                                class_name="chakra-image css-b85nkz",
-                            ),
-                            class_name="css-k008qs",
-                        ),
-                        class_name="css-lfcvwb",
-                    ),
-                    class_name="chakra-container css-16vsuek",
-                ),
+                features(),
                 rx.box(
                     rx.box(
                         rx.box(
