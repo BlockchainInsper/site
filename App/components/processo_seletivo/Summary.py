77fe0641fd5cf6f348ddd18d0f7aaeeae91aa5ce
matheusluciano99 <matheusluciano99@gmail.com>
2025-03-03 22:09:46 -0300

    falta ajeitar posicionamento, cor de fundo e o icone de projetos

--- App/components/processo_seletivo/Summary.py
+++ App/components/processo_seletivo/Summary.py
@@ -1,115 +1,78 @@
 import reflex as rx
 
 
-def summary():
-    return (
-        rx.box(
-            rx.box(
-                rx.el.a(
-                    rx.box(
-                        rx.box(
-                            rx.el.svg(
-                                rx.el.svg.path(
-                                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zm-141.651-35.33c4.937-32.999-20.191-50.739-54.55-62.573l11.146-44.702-27.213-6.781-10.851 43.524c-7.154-1.783-14.502-3.464-21.803-5.13l10.929-43.81-27.198-6.781-11.153 44.686c-5.922-1.349-11.735-2.682-17.377-4.084l.031-.14-37.53-9.37-7.239 29.062s20.191 4.627 19.765 4.913c11.022 2.751 13.014 10.044 12.68 15.825l-12.696 50.925c.76.194 1.744.473 2.829.907-.907-.225-1.876-.473-2.876-.713l-17.796 71.338c-1.349 3.348-4.767 8.37-12.471 6.464.271.395-19.78-4.937-19.78-4.937l-13.51 31.147 35.414 8.827c6.588 1.651 13.045 3.379 19.4 5.006l-11.262 45.213 27.182 6.781 11.153-44.733a1038.209 1038.209 0 0 0 21.687 5.627l-11.115 44.523 27.213 6.781 11.262-45.128c46.404 8.781 81.299 5.239 95.986-36.727 11.836-33.79-.589-53.281-25.004-65.991 17.78-4.098 31.174-15.792 34.747-39.949zm-62.177 87.179c-8.41 33.79-65.308 15.523-83.755 10.943l14.944-59.899c18.446 4.603 77.6 13.717 68.811 48.956zm8.417-87.667c-7.673 30.736-55.031 15.12-70.393 11.292l13.548-54.327c15.363 3.828 64.836 10.973 56.845 43.035z"
-                                ),
-                                stroke="currentColor",
-                                fill="currentColor",
-                                stroke_width="0",
-                                viewbox="0 0 512 512",
-                                height="1em",
-                                width="1em",
-                                xmlns="http://www.w3.org/2000/svg",
-                            ),
-                            class_name="css-1x6efgi",
-                        ),
-                        rx.box(
-                            rx.text(
-                                "N\u00facleos",
-                                class_name="chakra-text css-1jnffv6",
-                            ),
-                            rx.box(
-                                "Clique aqui para conhecer nossas \u00e1reas de estudos",
-                                class_name="css-9826r4",
-                            ),
-                            class_name="chakra-stack css-46p1lt",
-                        ),
-                        class_name="chakra-stack css-v3nyow",
-                    ),
-                    class_name="chakra-link css-f4h6uy",
-                    href="/areas",
-                ),
-                rx.el.a(
-                    rx.box(
-                        rx.box(
-                            rx.el.svg(
-                                rx.el.svg.path(fill="none", d="M0 0h24v24H0z"),
-                                rx.el.svg.path(
-                                    d="M17 11V3H7v4H3v14h8v-4h2v4h8V11h-4zM7 19H5v-2h2v2zm0-4H5v-2h2v2zm0-4H5V9h2v2zm4 4H9v-2h2v2zm0-4H9V9h2v2zm0-4H9V5h2v2zm4 8h-2v-2h2v2zm0-4h-2V9h2v2zm0-4h-2V5h2v2zm4 12h-2v-2h2v2zm0-4h-2v-2h2v2z"
-                                ),
-                                stroke="currentColor",
-                                fill="currentColor",
-                                stroke_width="0",
-                                viewbox="0 0 24 24",
-                                height="1em",
-                                width="1em",
-                                xmlns="http://www.w3.org/2000/svg",
-                            ),
-                            class_name="css-1x6efgi",
-                        ),
-                        rx.box(
-                            rx.text(
-                                "Parceiros",
-                                class_name="chakra-text css-1jnffv6",
-                            ),
-                            rx.box(
-                                "Clique aqui para conhecer nossos parceiros",
-                                class_name="css-9826r4",
-                            ),
-                            class_name="chakra-stack css-46p1lt",
-                        ),
-                        class_name="chakra-stack css-v3nyow",
-                    ),
-                    class_name="chakra-link css-f4h6uy",
-                    href="/partnerships",
-                ),
-                rx.el.a(
-                    rx.box(
-                        rx.box(
-                            rx.el.svg(
-                                rx.el.svg.defs(),
-                                rx.el.svg.path(
-                                    d="M312.1 591.5c3.1 3.1 8.2 3.1 11.3 0l101.8-101.8 86.1 86.2c3.1 3.1 8.2 3.1 11.3 0l226.3-226.5c3.1-3.1 3.1-8.2 0-11.3l-36.8-36.8c-3.1-3.1-8.2-3.1-11.3 0L517 485.3l-86.1-86.2c-3.1-3.1-8.2-3.1-11.3 0L275.3 543.4c-3.1 3.1-3.1 8.2 0 11.3l36.8 36.8z"
-                                ),
-                                rx.el.svg.path(
-                                    d="M904 160H548V96c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v64H120c-17.7 0-32 14.3-32 32v520c0 17.7 14.3 32 32 32h356.4v32L311.6 884.1c-3.7 2.4-4.7 7.3-2.3 11l30.3 47.2v0.1c2.4 3.7 7.4 4.7 11.1 2.3L512 838.9l161.3 105.8c3.7 2.4 8.7 1.4 11.1-2.3v-0.1l30.3-47.2c2.4-3.7 1.3-8.6-2.3-11L548 776.3V744h356c17.7 0 32-14.3 32-32V192c0-17.7-14.3-32-32-32z m-40 512H160V232h704v440z"
-                                ),
-                                stroke="currentColor",
-                                fill="currentColor",
-                                stroke_width="0",
-                                t="1569683753031",
-                                viewbox="0 0 1024 1024",
-                                version="1.1",
-                                height="1em",
-                                width="1em",
-                                xmlns="http://www.w3.org/2000/svg",
-                            ),
-                            class_name="css-1x6efgi",
-                        ),
-                        rx.box(
-                            rx.text("Projetos", class_name="chakra-text css-1jnffv6"),
-                            rx.box(
-                                "Clique aqui para conhecer nossos alguns dos nossos melhores projetos",
-                                class_name="css-9826r4",
-                            ),
-                            class_name="chakra-stack css-46p1lt",
-                        ),
-                        class_name="chakra-stack css-v3nyow",
-                    ),
-                    class_name="chakra-link css-f4h6uy",
-                    href="/projects",
-                ),
-                class_name="css-sj7xet",
-            ),
-            class_name="css-1u3waxk",
+def feature(title: str, children: str, icon: rx.Component):
+    return rx.stack(
+        rx.box(icon, class_name="text-6xl"),
+        rx.stack(
+            rx.text(title, class_name="font-extrabold text-lg"),
+            rx.box(children, class_name="text-gray-600 dark:text-gray-400"),
+            class_name="space-y-1",
         ),
+        class_name="flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-6",
     )
+
+
+def summary():
+    bitcoin_svg = rx.el.svg(
+        rx.el.svg.path(
+            d="M23.638 14.904c-1.602 6.43-8.113 10.34-14.542 8.736C2.67 22.05-1.244 15.525.362 9.105 1.962 2.67 8.475-1.243 14.9.358c6.43 1.605 10.342 8.115 8.738 14.548v-.002zm-6.35-4.613c.24-1.59-.974-2.45-2.64-3.03l.54-2.153-1.315-.33-.525 2.107c-.345-.087-.705-.167-1.064-.25l.526-2.127-1.32-.33-.54 2.165c-.285-.067-.565-.132-.84-.2l-1.815-.45-.35 1.407s.974.225.955.236c.535.136.63.486.615.766l-1.477 5.92c-.075.166-.24.406-.614.314.015.02-.96-.24-.96-.24l-.66 1.51 1.71.426.93.236-.54 2.19 1.32.327.54-2.17c.36.1.705.19 1.05.273l-.51 2.154 1.32.33.545-2.19c2.24.427 3.93.257 4.64-1.774.57-1.637-.03-2.58-1.217-3.196.854-.193 1.5-.76 1.68-1.93h.01zm-3.01 4.22c-.404 1.64-3.157.75-4.05.53l.72-2.9c.896.23 3.757.67 3.33 2.37zm.41-4.24c-.37 1.49-2.662.735-3.405.55l.654-2.64c.744.18 3.137.524 2.75 2.084v.006z",
+            fill="currentColor",
+        ),
+        view_box="0 0 24 24",
+        class_name="h-12 w-12",
+    )
+
+    building_svg = rx.el.svg(
+        rx.el.svg.path(
+            d="M17 11V3H7v4H3v14h8v-4h2v4h8V11h-4zM7 19H5v-2h2v2zm0-4H5v-2h2v2zm0-4H5V9h2v2zm4 4H9v-2h2v2zm0-4H9V9h2v2zm0-4H9V5h2v2zm4 8h-2v-2h2v2zm0-4h-2V9h2v2zm0-4h-2V5h2v2zm4 12h-2v-2h2v2zm0-4h-2v-2h2v2z",
+            fill="currentColor",
+        ),
+        view_box="0 0 24 24",
+        class_name="h-12 w-12",
+    )
+
+    projects_svg = rx.el.svg(
+        rx.el.svg.path(
+            d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9 17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z",
+            fill="currentColor",
+        ),
+        view_box="0 0 24 24",
+        class_name="h-12 w-12",
+    )
+
+    return rx.box(
+        rx.grid(
+            rx.link(
+                feature(
+                    "Núcleos",
+                    "Clique aqui para conhecer nossas áreas de estudos",
+                    bitcoin_svg,
+                ),
+                href="#/areas",
+            ),
+            rx.link(
+                feature(
+                    "Parceiros",
+                    "Clique aqui para conhecer nossos parceiros",
+                    building_svg,
+                ),
+                href="#/partnerships",
+            ),
+            rx.link(
+                feature(
+                    "Projetos",
+                    "Clique aqui para conhecer nossos alguns dos nossos melhores projetos",
+                    projects_svg,
+                ),
+                href="#/projects",
+            ),
+            class_name="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-8 md:gap-y-14",
+        ),
+        class_name="max-w-5xl mx-auto pt-4 pb-12 px-6 md:px-8",
+    )
+
+
+# Configuração do app
+app = rx.App()
+app.add_page(summary)
